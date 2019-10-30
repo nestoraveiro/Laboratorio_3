906cb2810ed4c6b4025b2afdb88f39b810624f27
arieldagnino <54157502+arieldagnino@users.noreply.github.com>
2019-10-29 21:21:12 -0300

    hasta 3.c sin probar en placa

--- main.c
+++ main.c
@@ -1,64 +1,10 @@
-/**holaa nestor
-  Generated main.c file from MPLAB Code Configurator
 
-  @Company
-    Microchip Technology Inc.
-
-  @File Name
-    main.c
-
-  @Summary
-    This is the generated main.c using PIC24 / dsPIC33 / PIC32MM MCUs.
-
-  @Description
-    This source file provides main entry point for system initialization and application code development.
-    Generation Information :
-        Product Revision  :  PIC24 / dsPIC33 / PIC32MM MCUs - 1.145.0
-        Device            :  PIC32MM0256GPM064
-    The generated drivers are tested against the following:
-        Compiler          :  XC16 v1.36b
-        MPLAB 	          :  MPLAB X v5.25
-*/
-
-/*
-    (c) 2019 Microchip Technology Inc. and its subsidiaries. You may use this
-    software and any derivatives exclusively with Microchip products.
-
-    THIS SOFTWARE IS SUPPLIED BY MICROCHIP "AS IS". NO WARRANTIES, WHETHER
-    EXPRESS, IMPLIED OR STATUTORY, APPLY TO THIS SOFTWARE, INCLUDING ANY IMPLIED
-    WARRANTIES OF NON-INFRINGEMENT, MERCHANTABILITY, AND FITNESS FOR A
-    PARTICULAR PURPOSE, OR ITS INTERACTION WITH MICROCHIP PRODUCTS, COMBINATION
-    WITH ANY OTHER PRODUCTS, OR USE IN ANY APPLICATION.
-
-    IN NO EVENT WILL MICROCHIP BE LIABLE FOR ANY INDIRECT, SPECIAL, PUNITIVE,
-    INCIDENTAL OR CONSEQUENTIAL LOSS, DAMAGE, COST OR EXPENSE OF ANY KIND
-    WHATSOEVER RELATED TO THE SOFTWARE, HOWEVER CAUSED, EVEN IF MICROCHIP HAS
-    BEEN ADVISED OF THE POSSIBILITY OR THE DAMAGES ARE FORESEEABLE. TO THE
-    FULLEST EXTENT ALLOWED BY LAW, MICROCHIP'S TOTAL LIABILITY ON ALL CLAIMS IN
-    ANY WAY RELATED TO THIS SOFTWARE WILL NOT EXCEED THE AMOUNT OF FEES, IF ANY,
-    THAT YOU HAVE PAID DIRECTLY TO MICROCHIP FOR THIS SOFTWARE.
-
-    MICROCHIP PROVIDES THIS SOFTWARE CONDITIONALLY UPON YOUR ACCEPTANCE OF THESE
-    TERMS.
-*/
-
-/**
-  Section: Included Files
-*/
 #include <stdio.h>
 #include <stdint.h>
 #include "mcc_generated_files/system.h"
 #include "mcc_generated_files/pin_manager.h" 
 #include "mcc_generated_files/tmr2.h"
 #include  "mcc_generated_files/usb/usb.h"
-
-
-
-
-
-/*
-                         Main application
- */
 
 
 int main(void)
@@ -83,20 +29,37 @@ int main(void)
             LEDA_SetLow();
             
         }
-        if(USBUSARTIsTxTrfReady())
-        {
-            char data[] = "Hello World";
-            putsUSBUSART(data);
-        }
+        
+ //*****************************__USB__*****************************************
+ //_____________________________________________________________________________
+        
+        uint8_t numBytes;
+        uint8_t buffer[64];
+        
+        USBDeviceTasks(); //Takes care of enumeration and other USB events
+            if((USBGetDeviceState() != CONFIGURED_STATE) ||
+               (USBIsDeviceSuspended() == true))
+            {
+                continue;   //go back to the top of the while loop
+            }
+            else
+            {
+                int i;
+                for(i = 0;i < 64;i++)
+                {
+                buffer[i] = 0;
+                }
+                numBytes = getsUSBUSART(buffer,sizeof(buffer)); //until the buffer is free.
+                if(numBytes > 0)
+                {
+                
+                 putsUSBUSART(buffer);
+                   
+                }
+            }
         CDCTxService();
     }
-        
-        
-        // Add your application code
     
     return 1; 
 }
-/**
- End of File
-*/
 
